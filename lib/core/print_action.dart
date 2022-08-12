8c6bdf07523aae380c30b119357f64b092334cc3
Phuong Nam <phuongnam1952001@gmail.com>
2022-08-13 00:01:59 +0700

    color threshold

--- lib/core/print_action.dart
+++ lib/core/print_action.dart
@@ -95,7 +95,7 @@ class PrintAction {
           int newColor = 1;
           if ((0.299 * color.red + 0.587 * color.green + 0.114 * color.blue) /
                   255 <
-              0.85) {
+              0.8) {
             newColor = 0;
           }
           bit = (bit << 1) ^ newColor;
